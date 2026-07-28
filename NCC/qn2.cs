16ce1b51e50870aacc6a6e7bfa05b42e2ad59b39
Kamanaaaa <158715235+Kamanaaaa@users.noreply.github.com>
2026-07-28 12:05:24 +0545

    Add files via upload

---
+++ NCC/qn2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Shape
+{
+    public virtual void Area()
+    {
+        Console.WriteLine("Area of Shape");
+    }
+}
+
+class Circle : Shape
+{
+    private double radius;
+
+    public Circle(double r)
+    {
+        radius = r;
+    }
+
+    public override void Area()
+    {
+        double area = 3.14 * radius * radius;
+        Console.WriteLine("Area of Circle = " + area);
+    }
+}
+
+class Rectangle : Shape
+{
+    private double length;
+    private double breadth;
+
+    public Rectangle(double l, double b)
+    {
+        length = l;
+        breadth = b;
+    }
+
+    public override void Area()
+    {
+        double area = length * breadth;
+        Console.WriteLine("Area of Rectangle = " + area);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Base class reference pointing to Circle object
+        Shape s;
+
+        s = new Circle(5);
+        s.Area();
+
+        // Base class reference pointing to Rectangle object
+        s = new Rectangle(6, 4);
+        s.Area();
+
+        Console.ReadLine();
+    }
+}
---
+++ NCC/qn3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class week
+{
+    private string[] days =
+    {
+        "sunday",
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "saturday"
+    };
+    public string this[int index]
+    {
+        get
+        {
+            if (index >= 0 && index < days.Length)
+                return days[index];
+            else
+                return "invalid index";
+        }
+        set
+        {
+            if(index>=0&& index < days.Length)
+                days[index] = value;
+        }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        week week=new week();
+        Console.WriteLine(week[0]);
+        Console.WriteLine(week[1]);
+        Console.WriteLine(week[2]);
+    }
+}
