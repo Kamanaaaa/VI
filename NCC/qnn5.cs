6eb9f8e0414db6f7c5f2f35753f13da30bf6f3a3
Kamanaaaa <158715235+Kamanaaaa@users.noreply.github.com>
2026-08-12 16:39:36 +0545

    Add files via upload

---
+++ NCC/qnn5.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+abstract class Employee
+{
+    public string Name;
+
+    public Employee(string name)
+    {
+        Name = name;
+    }
+
+    // Abstract method
+    public abstract double CalculateSalary();
+}
+
+class Manager : Employee
+{
+    public Manager(string name) : base(name)
+    {
+    }
+
+    public override double CalculateSalary()
+    {
+        return 50000;
+    }
+}
+
+class Clerk : Employee
+{
+    public Clerk(string name) : base(name)
+    {
+    }
+
+    public override double CalculateSalary()
+    {
+        return 25000;
+    }
+}
+
+class EmployeeTest
+{
+    static void Main(string[] args)
+    {
+        Employee manager = new Manager("Kamana");
+        Employee clerk = new Clerk("Manila");
+
+        Console.WriteLine("Employee Name: " + manager.Name);
+        Console.WriteLine("Salary: " + manager.CalculateSalary());
+
+        Console.WriteLine();
+
+        Console.WriteLine("Employee Name: " + clerk.Name);
+        Console.WriteLine("Salary: " + clerk.CalculateSalary());
+    }
+}
