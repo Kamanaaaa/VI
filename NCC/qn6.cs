91a22adc7c195f4a947f950bb45f5657744db365
Kamanaaaa <158715235+Kamanaaaa@users.noreply.github.com>
2026-08-18 12:26:19 +0545

    Add files via upload

---
+++ NCC/qn6.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+interface IPayable
+{
+    void Pay();
+}
+class Invoice: IPayable
+{
+    public void Pay()
+    {
+        Console.WriteLine("Payment made for Invoice");
+    }
+}
+class salary: IPayable
+{
+    public void Pay()
+    {
+        Console.WriteLine("salary payment processed.");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        List<IPayable> payments = new List<IPayable>();
+        payments.Add(new Invoice());
+        payments.Add(new salary());
+
+        foreach (IPayable payment in payments)
+        {
+            payment.Pay();
+        }
+    }
+}
---
+++ NCC/qn7.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class TemperatureSensor
+{
+    public delegate void TemperatureAlertHandler(string message);
+
+    public event TemperatureAlertHandler TemperatureExceeded;
+    private double threshold = 40;
+
+    public void checkTemperature(double temperature)
+    {
+        Console.WriteLine("Current Temperature:" + temperature);
+        if (temperature > threshold)
+        {
+            TemperatureExceeded?.Invoke("Alert! temperature has crossed the threshold.");
+        }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        TemperatureSensor sensor = new TemperatureSensor();
+        sensor.TemperatureExceeded += ShowAlert;
+        sensor.checkTemperature(35);
+        sensor.checkTemperature(42);
+
+    }
+    static void ShowAlert(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
